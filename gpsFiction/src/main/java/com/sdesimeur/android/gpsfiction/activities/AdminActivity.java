49673781afeaec16c39150f7bb3bd8b15821276c
sdesimeur <pro@sdesimeur.com>
2016-11-14 18:26:07 +0100

    add AdminActivity (p2)

--- gpsFiction/src/main/java/com/sdesimeur/android/gpsfiction/activities/AdminActivity.java
+++ gpsFiction/src/main/java/com/sdesimeur/android/gpsfiction/activities/AdminActivity.java
@@ -1,11 +1,14 @@
 package com.sdesimeur.android.gpsfiction.activities;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.sdesimeur.android.gpsfiction.R;
 
@@ -22,6 +25,8 @@ import java.util.Set;
  */
 public class AdminActivity extends Activity {
     private HashMap <String, Locale> string2locale = new HashMap<>();
+    private Spinner languageLocaleSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -29,7 +34,7 @@ public class AdminActivity extends Activity {
         super.onCreate(savedInstanceState);
        // mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LOW_PROFILE|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.adminactivity);
-        Spinner lls = (Spinner) findViewById(R.id.LanguageListSpinner);
+        languageLocaleSpinner = (Spinner) findViewById(R.id.LanguageListSpinner);
         Set<String> codeCountryArray = new HashSet<>();
         Collections.addAll(codeCountryArray,getResources().getStringArray(R.array.countryCodeArray));
         /*
@@ -54,15 +59,22 @@ public class AdminActivity extends Activity {
         Collections.sort(langs, String.CASE_INSENSITIVE_ORDER);
 
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.spinnerlanguageselect, langs);
-        lls.setAdapter(adapter);
-
-        lls.setSelection(i);
+        languageLocaleSpinner.setAdapter(adapter);
+        languageLocaleSpinner.setSelection(i);
     }
 
     public void changeAdminPassword(View v) {
 
     }
     public void startGames ( View v) {
-
+        Locale l = string2locale.get(languageLocaleSpinner.getSelectedItem());
+        Locale.setDefault(l);
+        Configuration cfg = getResources().getConfiguration();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            cfg.setLocale(l);
+        }
+        TextView tv = (TextView) findViewById(R.id.mytext);
+        tv.setText(getResources().getString(R.string.titleCompass));
+        tv.invalidate();
     }
 }
